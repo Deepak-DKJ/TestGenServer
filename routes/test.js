86ec1c967bea10817999833a6697d5901cc7c171
Deepak-DKJ <deepakjha1104@gmail.com>
2025-07-20 01:39:31 +0530

    changed model from 2.5 pro to flash for preventing vercel timeout

--- routes/test.js
+++ routes/test.js
@@ -19,7 +19,7 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
-const model = genAI.getGenerativeModel({ model: "models/gemini-2.5-pro" });
+const model = genAI.getGenerativeModel({ model: "models/gemini-2.5-flash" });
 const multer = require("multer");
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
